b49c6974cf62991785e4517d286ea4368d25e0d2
AntaresNebula <buer19970329@gmail.com>
2021-10-05 11:33:40 +0800

    Add files via upload

---
+++ helperfunctions/npz2mat.py
@@ -0,0 +1,30 @@
+# %%
+import numpy as np
+import scipy.io as io
+import os
+
+def npz2mat(data='full_path'):
+    """
+    Usage
+    ----------
+    convert .npz data file to .mat data file
+
+    Parameters
+    ----------
+    data: full path of data
+    
+    Example:
+    ----------
+    import npz2mat
+    npz2mat('/Users/linxiaomin/Desktop/LINIP/Python/GUI/Tool-for-Computing-DynamicFC-main/test_data/dfc_dfc_output.npz')
+    
+    
+    """
+    # extract data path
+    data_path = data.split('/')
+    data_name = data_path[-1].split('.')[0] +'.mat'
+    data_path.pop(-1)
+    data_path = '/'.join(data_path)
+    # convert npz to mat
+    npz = np.load(data)
+    io.savemat(os.path.join(data_path,data_name),{'data':npz})
